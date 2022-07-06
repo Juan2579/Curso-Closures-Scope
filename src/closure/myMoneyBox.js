463234d13714390049cef19a6c03a376132c1d00
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-07-05 23:52:14 -0500

    Final commi with last exercisex

--- src/closure/myMoneyBox.js
+++ src/closure/myMoneyBox.js
@@ -11,7 +11,7 @@ function moneyBox() {
     let saveCoins = 0;
     function countCoins(coins) {
         saveCoins += coins
-        console.log(`MoneyBox: $${countCoins}`)
+        console.log(`MoneyBox: $${saveCoins}`)
     }
     return countCoins
 }
@@ -20,3 +20,8 @@ const myMoneyBox = moneyBox()
 myMoneyBox(5)
 myMoneyBox(5)
 myMoneyBox(15)
+
+const moneyBoxAna = moneyBox();
+moneyBoxAna(10)
+moneyBoxAna(20)
+moneyBoxAna(5)
---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+// var nameOfDog; //undefined
+
+// console.log(nameOfDog)
+// var nameOfDog = "Elmo"
+// console.log
+
+var elmo; // undefinded
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`)
+}
+
+var elmo = "Elmito"
